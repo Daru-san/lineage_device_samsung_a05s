ef272f4a49e010b6560858261543d721cdbedf45
Daru <mica.tui@proton.me>
2025-11-25 22:35:17 +0200

    fix: add missing device configurations

--- device.mk
+++ device.mk
@@ -11,5 +11,19 @@ $(call inherit-product, device/samsung/bengal-common/bengal.mk)
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Local overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
+
+# Audio
+PRODUCT_COPY_FILES += \
+	$(DEVICE_PATH)/audio/configs/mixer_paths_bengal_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_bengal_qrd.xml \
+	$(DEVICE_PATH)/audio/configs/mixer_paths_scubaidp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_scubaidp.xml \
+	$(DEVICE_PATH)/audio/configs/mixer_paths_scubaqrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_scubaqrd.xml \
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/samsung/bengal/bengal-vendor.mk)
