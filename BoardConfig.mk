b101a723279dca87de9126936c90ac4e8b9e4e76
Daru <mica.tui@proton.me>
2025-11-25 17:06:46 +0200

    refactor: add target kernel config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,6 +5,7 @@ TARGET_KERNEL_SOURCE        := kernel/samsung/bengal
 TARGET_KERNEL_ARCH          := arm64
 TARGET_KERNEL_HEADER_ARCH   := arm64
 TARGET_LINUX_KERNEL_VERSION := 5.15
+TARGET_KERNEL_CONFIG := gki_defconfig
 
 # Inherit from common tree
 include device/samsung/bengal-common/BoardConfigCommon.mk
